50715fc99c80713d7e527720b80df20290c8c4cb
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-08-11 14:46:28 -0300

    feat: Adds Footer component

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="page-footer">
+      <span>Bitcoin Node Activity Panel • Rede Signet</span>
+      <span>Desenvolvido com foco em performance e minimalismo</span>
+    </footer>
+  );
+}

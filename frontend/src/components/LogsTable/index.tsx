f7888166931366724cab7b48c22f0318b71e8d53
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-08-26 18:23:38 -0300

    feat: Creates the LogsTable component to structure the logs table

---
+++ frontend/src/components/LogsTable/index.tsx
@@ -0,0 +1,11 @@
+import LogsTableBody from "./LogsTableBody";
+import LogsTableBodyEmpty from "./LogsTableBodyEmpty";
+import LogsTableHead from "./LogsTableHead";
+import LogsTableRoot from "./LogsTableRoot";
+
+export const LogsTable = {
+    Body: LogsTableBody,
+    BodyEmpty: LogsTableBodyEmpty,
+    Head: LogsTableHead,
+    Root: LogsTableRoot,
+};

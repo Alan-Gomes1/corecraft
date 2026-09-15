05484e8c36d079a436cc2e33181afd7aeede511a
Alan Gomes <alan.gomes.ag28@gmail.com>
2026-09-15 18:07:48 -0300

    refactor: Fixes CardCompare import and adds CardChart and CardStatistic to the Card component

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,11 +1,15 @@
 import CardDetail from "./CardDetail";
 import CardItem from "./CardItem";
 import CardRoot from "./CardRoot";
-import CardCompare from "./CradCompare";
+import CardCompare from "./CardCompare";
+import CardChart from "./CardChart";
+import CardStatistic from "./CardStatistic";
 
 export const Card = {
   Root: CardRoot,
   Item: CardItem,
   Detail: CardDetail,
   Compare: CardCompare,
+  Chart: CardChart,
+  Statistic: CardStatistic,
 };
